32ce7d24ea50e59da29298796504a3d7308ed7da
jnull <admin@jarednull.com>
2017-03-29 04:30:08 -0700

    better formated code on client.js

--- src/js/client.js
+++ src/js/client.js
@@ -4,42 +4,42 @@ import {HashRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
 const MainNavLayout = ({match}) => (
     <div>
-        <h2>Accounts</h2>
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/person/netflix">Bob Hope</Link>
-            </li>
-            <li>
-                <Link to="/person/zillow-group">Charlie</Link>
-            </li>
-            <li>
-                <Link to="/company/yahoo">Yahoo</Link>
-            </li>
-            <li>
-                <Link to="/company/modus-create">Modus Create</Link>
-            </li>
+      <h2>Accounts</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/person/netflix">Bob Hope</Link>
+        </li>
+        <li>
+          <Link to="/person/zillow-group">Charlie</Link>
+        </li>
+        <li>
+          <Link to="/company/yahoo">Yahoo</Link>
+        </li>
+        <li>
+          <Link to="/company/modus-create">Modus Create</Link>
+        </li>
         </ul>
     </div>
 )
 
 const Person = ({match}) => (
     <div>
-        <h3>Person: {match.params.id}</h3>
+      <h3>Person: {match.params.id}</h3>
     </div>
 )
 
 const Company = ({match}) => (
     <div>
-        <h3>Company: {match.params.id}</h3>
+      <h3>Company: {match.params.id}</h3>
     </div>
 )
 
 const NoMatch = ({match}) => (
     <div>
-        <h3>NoMatch</h3>
+      <h3>NoMatch</h3>
     </div>
 )
 
@@ -52,14 +52,14 @@ class App extends React.Component {
     render() {
         return (
             <Router>
-                <div>
-                    <Route path="/" component={MainNavLayout}/>
-                    <Switch>
-                        <Route path="/person/:id" component={Person}/>
-                        <Route path="/company/:id" component={Company}/>
-                        <Route component={NoMatch}/>
-                    </Switch>
-                </div>
+              <div>
+                <Route path="/" component={MainNavLayout}/>
+                <Switch>
+                  <Route path="/person/:id" component={Person}/>
+                  <Route path="/company/:id" component={Company}/>
+                  <Route component={NoMatch}/>
+                </Switch>
+              </div>
             </Router>
         );
     }
